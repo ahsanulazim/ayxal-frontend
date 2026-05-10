3f631539a56ffaac3d3524bac00559851a560eff
Ahsanul Azim Plabon <ahsanulazim6@gmail.com>
2026-05-11 02:27:30 +0600

    little optimization on the fonts

--- components/home/Categories.jsx
+++ components/home/Categories.jsx
@@ -5,7 +5,9 @@ const Categories = () => {
   return (
     <section className="px-5 mb-5">
       <div className="max-w-360 mx-auto">
-        <h2 className="text-3xl font-bold text-main">Popular Categories</h2>
+        <h2 className="text-xl sm:text-3xl font-bold text-main">
+          Popular Categories
+        </h2>
         <div className="flex justify-between gap-5 mt-5">
           {categories.map((category) => (
             <CategoryCard key={category.title} category={category} />
--- components/home/NewArrival.jsx
+++ components/home/NewArrival.jsx
@@ -4,7 +4,9 @@ const NewArrival = () => {
   return (
     <section className="px-5 mb-5">
       <div className="max-w-360 mx-auto">
-        <h2 className="text-3xl font-bold text-main">New Arrivals</h2>
+        <h2 className="text-xl sm:text-3xl font-bold text-main">
+          New Arrivals
+        </h2>
         <div className="mt-5 grid grid-cols-2 xs:grid-cols-3 lg:grid-cols-6 gap-5">
           <ProductCard />
           <ProductCard />
